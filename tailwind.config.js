39e1243620b949624308ece8e776b8850a76e33f
nawahtkui <alnwyra@gmail.com>
2025-11-01 03:21:56 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nawah: {
+          gold: '#C99A3B',
+          dark: '#0b0b0b',
+          darkMuted: '#121212',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
